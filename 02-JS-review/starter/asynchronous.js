d3620bfb8597bfbd5ca1e20c052bb8034aca0c62
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-01-29 01:18:14 +0800

    added another notes for promise, async, and await

--- 02-JS-review/starter/asynchronous.js
+++ 02-JS-review/starter/asynchronous.js
@@ -157,13 +157,20 @@ function getTotalReviewsCount(book) {
 // Asynchronous Javascript
 
 // promises
+// js process is synchronous line by line of code and when the process of a certain line takes time before producing an intended output -
+// it will be skipped and will proceed on reading the next line of code.
+// this kind of instance of code is called promise
+// when a promise statement finished loading/processing, we can execute a certain operation or function after it -
+// by using .then() method and then method can be chained
 fetch("https://jsonplaceholder.typicode.com/todos/1")
   .then((res) => res.json())
   .then((data) => console.log(data));
-
 console.log("other process");
 
 // async/await
+// async function should be created in order to put an await property inside it
+// await property makes a certain line of code of promise just like normal code means js will wait for it to finished processing -
+// before proceeding into next line.
 async function getTodos() {
   const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
   const data = await res.json();
